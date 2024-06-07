93e1cacab5660a22ea850905d42e53947b817d7e
hcherif19 <48697898+hcherif19@users.noreply.github.com>
2024-06-07 04:19:11 +0100

    user Resource and channels

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,22 +1,71 @@
-import { usePage } from '@inertiajs/react';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { usePage } from "@inertiajs/react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { useEffect, useState } from "react";
 
-
-const ChatLayout= ({children }) =>{
+const ChatLayout = ({ children }) => {
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
     console.log("conversations", conversations);
     console.log("selectedConversation", selectedConversation);
+    const [localConversations, setLocalConversations] = useState([]);
+    const [sortedConversations, setSortedConversations] = useState([]);
+    const [onlineUsers, setOnlineUsers] = useState({});
+    const isUserOnline = (userId) => onlineUsers[userId];
+    useEffect(() => {
+        setLocalConversations(conversations);
+    }, [conversations]);
 
-    return(
-<AuthenticatedLayout>
-    ChatLayout
-    <div>
-        {children}
-    </div>
-</AuthenticatedLayout>
+    useEffect(() => {
+        setSortedConversations(
+            localConversations.sort((a, b) => {
+                return new Date(b.updated_at) - new Date(a.updated_at);
+            })
+        );
+    }, [localConversations]);
+    
+    useEffect(() => {
+        Echo.join("online")
+            .here((users) => {
+                console.log("here", users);
+                const onlineUsersObject = Object.fromEntries(
+                    users.map((user) => [user.id, user])
+                );
+                setOnlineUsers((prevOnlineUsers) => {
+                    return { ...prevOnlineUsers, ...onlineUsersObject };
+                });
+            })
+            .joining((user) => {
+                console.log("joining", user);
+
+                setOnlineUsers((prevOnlineUsers) => {
+                    const updatedUsers = { ...prevOnlineUsers };
+                    updatedUsers[user.id] = user; // add the new user to the list
+                    return updatedUsers;
+                });
+            })
+            .leaving((user) => {
+                console.log("leaving", user);
+
+                setOnlineUsers((prevOnlineUsers) => {
+                    const updatedUsers = { ...prevOnlineUsers };
+                    delete updatedUsers[user.id]; // remove the user from the list
+                    return updatedUsers;
+                });
+            })
+            .error((error) => {
+                console.error("error : ", error);
+            });
+        return () => {
+            Echo.leave("online");
+        };
+    }, []);
+    return (
+        <AuthenticatedLayout>
+            ChatLayout
+            <div>{children}</div>
+        </AuthenticatedLayout>
     );
-}
+};
 
 export default ChatLayout;
